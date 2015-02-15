3cc14a846a70a8b1d0e0eb9ccc56935041682618
Vance Miller <vbmiller@cs.unc.edu>
2015-02-15 16:35:21 -0500

    cats in space

---
+++ unity/Assets/movement.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class movement : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	float speed = 1.0f;
+
+	// Update is called once per frame
+	void Update() {
+		var move = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
+		transform.position += move * speed * Time.deltaTime;
+	}
+}

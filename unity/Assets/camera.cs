6a3c3c14e3ca530bee2b5abb7ac4a17c211b2c4e
Adam Aji <adam.m.aji@lmco.com>
2015-02-26 19:48:07 -0500

    Start single planet gravitation

---
+++ unity/Assets/camera.cs
@@ -0,0 +1,27 @@
+﻿
+using UnityEngine;
+using System.Collections;
+
+public class camera : MonoBehaviour {
+
+	public Transform target;
+	public float distance;
+
+	void Start() {
+
+	}
+
+	void Update(){
+
+		Vector3 temp = transform.position;
+
+		if (Vector3.Distance (temp, target.position) > 10) {
+			temp.x = target.position.x;
+			temp.y = target.position.y;
+			temp.z = target.position.z - distance;
+			transform.position = Vector3.Lerp(transform.position, temp, 1f);
+		}
+		
+	}
+
+}
---
+++ unity/Assets/movement.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class movement : MonoBehaviour {
+	
+//	public float speed = 1.0f;
+//
+//	// Use this for initialization
+//	void Start () {
+//	
+//	}
+//
+//	// Update is called once per frame
+//	void Update() {
+//		var move = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
+//		transform.position += move * speed * Time.deltaTime;
+//	}
+
+	
+	private float moveSpeed = 5;
+	private Vector3 moveDirection;
+	
+	
+	void Update() {
+		moveDirection = new Vector3(Input.GetAxisRaw("Horizontal"),0,0).normalized;
+	}
+	
+	void FixedUpdate() {
+		rigidbody.MovePosition(rigidbody.position + transform.TransformDirection(moveDirection) * moveSpeed * Time.deltaTime);
+	}
+}

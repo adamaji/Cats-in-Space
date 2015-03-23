8c2a745f464f81be5d9e61685730d5192625ae35
Vance Miller <vbmiller@cs.unc.edu>
2015-03-23 19:04:47 -0400

    New build in /builds and new gameplay idea

---
+++ Cats_unity_project/Assets/Scripts/addable_behavior.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class addable_behavior : MonoBehaviour {
+	public int order_in_layer;
+	public KeyCode my_trigger;
+
+	private SpriteRenderer my_renderer;
+	private AudioSource my_audio;
+
+	// Use this for initialization
+	void Start () {
+		my_renderer = gameObject.GetComponent<SpriteRenderer> ();
+		my_renderer.enabled = false;
+		my_audio = gameObject.GetComponent<AudioSource> ();
+		my_audio.volume = 0;
+		my_renderer.sortingOrder = order_in_layer;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (my_trigger)) {
+			if (my_renderer.enabled) {
+				my_audio.volume = 0.0f;
+				my_renderer.enabled = false;
+			} else {
+				my_audio.volume = 1.0f;
+				my_renderer.enabled = true;
+			}
+		}
+	
+	}
+}

c26a0f04c8173ac978afb9e5e107022198635c35
Vance Miller <vbmiller@cs.unc.edu>
2015-04-14 19:00:32 -0400

    Working on making rotating planets with sound and puzzle pieces.
    See circle_top_half.png and circle_bottom_half.png for example pieces.

---
+++ Cats_unity_project/Assets/Scripts/sound_trigger.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class sound_trigger : MonoBehaviour {
+	public AudioSource audio;
+
+	// Use this for initialization
+	void Start () {
+		audio = gameObject.GetComponent<AudioSource> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void onTriggerEnter(Collider otherObject) {
+		audio.Play ();
+	}
+}

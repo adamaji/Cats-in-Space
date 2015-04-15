c0e48793a011c2486d9a181e22cac3b44ba185f1
Vance Miller <vbmiller@cs.unc.edu>
2015-04-14 20:13:48 -0400

    trigger attempts

---
+++ Cats_unity_project/Assets/Scripts/rotate.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class rotate : MonoBehaviour {
+	public Rigidbody2D rb2D;
+	private float speed = 5f;
+
+	void Start() {
+		rb2D = GetComponent<Rigidbody2D>();
+	}
+	
+	void FixedUpdate() {
+		rb2D.MoveRotation(rb2D.rotation + speed * Time.fixedDeltaTime);
+	}
+}
---
+++ Cats_unity_project/Assets/Scripts/trigger_script.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class trigger_script : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log ("initialized trigger script\n");
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter2D(Collider2D other) {
+		Debug.Log ("triggered\n");
+	}
+}

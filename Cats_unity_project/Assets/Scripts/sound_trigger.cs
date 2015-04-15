cc94da2dec6026a7ba8cc75702c583ce30c1718c
Vance Miller <vbmiller@cs.unc.edu>
2015-04-14 20:11:42 -0400

    Trying to make it work....

--- Cats_unity_project/Assets/Scripts/sound_trigger.cs
+++ Cats_unity_project/Assets/Scripts/sound_trigger.cs
@@ -7,6 +7,7 @@ public class sound_trigger : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		audio = gameObject.GetComponent<AudioSource> ();
+		print ("initialized sound trigger\n");
 	}
 	
 	// Update is called once per frame
@@ -14,7 +15,29 @@ public class sound_trigger : MonoBehaviour {
 	
 	}
 
-	void onTriggerEnter(Collider otherObject) {
+	void onTriggerEnter2D(Collider2D otherObject) {
+		print ("collided\n");
 		audio.Play ();
+	}
+
+	void onTriggerStay2D(Collider2D other) {
+		print ("stay\n");
+	}
+
+	void onTriggerLeave2D(Collider2D other) {
+		print ("leave\n");
+	}
+
+	void onCollisionEnter2D(Collision2D otherObject) {
+		print ("collided\n");
+		audio.Play ();
+	}
+
+	void onCollisionStay2D(Collision2D other) {
+		print ("stay\n");
+	}
+
+	void onCollisionLeave2D(Collision2D other) {
+		print ("leave\n");
 	}
 }

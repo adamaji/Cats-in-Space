135fa30c816db675e4f02e37f6532a1160232a1a
Vance Miller <vbmiller@cs.unc.edu>
2015-04-27 21:05:40 -0400

    recycle bin and level select

---
+++ Cats_unity_project/Assets/Scripts/LevelSelect.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelSelect : MonoBehaviour {
+	public int level;
+
+	public enum Fade
+	{
+		In,
+		Out
+	};
+	
+	public void resetLevel() {
+		FadeAudio (4.0f, Fade.Out);
+		Debug.Log ("asdf");
+		Application.LoadLevel (level);
+	}
+
+	IEnumerator FadeAudio (float timer, Fade fadeType) {
+		float start = fadeType == Fade.In ? 0.0F : 1.0F;
+		float end = fadeType == Fade.In ? 1.1F : 0.0F;
+		float i = 0.0F;
+		float step = 1.0F / timer;
+		while (i <= 1.0F) {
+			Debug.Log (i);
+			i += step * Time.deltaTime;
+			GameObject.Find ("Audio Source").GetComponent<AudioSource>().volume = Mathf.Lerp (start, end, i);
+			yield return new WaitForSeconds(step * Time.deltaTime);
+		}
+	}
+}
--- Cats_unity_project/Assets/Scripts/WorldObject.cs
+++ Cats_unity_project/Assets/Scripts/WorldObject.cs
@@ -4,9 +4,12 @@ using System.Collections;
 public class WorldObject : MonoBehaviour {
 
 	public GameObject baseObject;
-	public float initialRotationDeg = 0;
+	public float initialRotationDeg = -45;
 	Vector3 rotation;
 	private float padding = 0.01f;
+	private Vector3 startPos;
+	private Vector3 startAng;
+	private Collider currentColliding;
 
 	void Start() {
 		this.baseObject = GameObject.Find ("base");
@@ -20,15 +23,49 @@ public class WorldObject : MonoBehaviour {
 		} else {
 			Debug.LogError("Sprite not found at " + imageDir, this);
 		}
+		initialRotationDeg = -45;
 	}
 
+	void OnMouseDown() {
+		startPos = gameObject.transform.position;
+		startAng = gameObject.transform.eulerAngles;
+	}
+	
 	void OnMouseDrag() {
+		Vector3 pos = Camera.main.ScreenToWorldPoint (Input.mousePosition );
+		pos.z = gameObject.transform.position.z;
+		transform.position = pos;
 		Vector3 v3 = Camera.main.WorldToScreenPoint(baseObject.transform.position);
 		v3 = Input.mousePosition - v3;
-		float angle = ((Mathf.Atan2( v3.y, v3.x) * Mathf.Rad2Deg) - 90 + initialRotationDeg) % 360 ;
-		rotation = new Vector3(0.0f, 0.0f, angle * padding);
-
-		//gameObject.transform.eulerAngles = rotation;
-		gameObject.transform.RotateAround(new Vector3 (0, 0, 0), new Vector3 (0, 0, 1), rotation.z);
+		float angle = ((Mathf.Atan2( v3.y, v3.x)* Mathf.Rad2Deg) - 90 + initialRotationDeg) % 360 ;
+		rotation = new Vector3(0.0f,0.0f,angle);
+		
+		gameObject.transform.eulerAngles = rotation;
+		//gameObject.transform.RotateAround(new Vector3 (0, 0, 0), new Vector3 (0, 0, 1), rotation.z);
+	}
+	
+	void OnMouseUp() {
+		BoxCollider pos = GameObject.Find ("recyclebin").GetComponent<BoxCollider> ();
+		Debug.Log (pos.bounds);
+		Debug.Log (Input.mousePosition);
+		if (pos.bounds.Contains(Input.mousePosition)) {
+			Destroy (this.gameObject);
+		} else {
+			gameObject.transform.position = startPos;
+			gameObject.transform.eulerAngles = startAng;
+			gameObject.transform.RotateAround (this.baseObject.transform.position, new Vector3 (0, 0, 1), rotation.z - startAng.z);
+		}
+	}
+	
+	void OnTriggerEnter(Collider other) {
+		if (other.name == "recyclebin") {
+			currentColliding = other;
+		}
+	}
+	
+	void OnTriggerExit(Collider exit) {
+		if (exit.name == "recyclebin") {
+			currentColliding = null;
+		}
 	}
 }

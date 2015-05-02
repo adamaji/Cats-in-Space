0aa87ff1a1e2de27d561c8eec56ac64c27cbdb03
Adam Aji <adam.m.aji@lmco.com>
2015-05-01 21:13:47 -0400

    Add prelim help button

---
+++ Cats_unity_project/Assets/Scripts/HelpScreen.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HelpScreen : MonoBehaviour {
+
+	void Start(){
+		GetComponent<GUITexture>().pixelInset = new Rect(0f, 0f, 0f, 0f);
+		transform.localScale = new Vector3 (1f, 1f, 1f);
+		transform.position = new Vector2 (0.5f, 0.5f);
+		//GetComponent<SpriteRenderer> ().enabled = false;
+		GetComponent<GUITexture> ().enabled = false;
+	}
+
+	public void ToggleScreen() {
+		GetComponent<GUITexture> ().enabled = !GetComponent<GUITexture> ().enabled;
+	}
+}
---
+++ Cats_unity_project/Assets/Scripts/HoverBehavior.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class HoverBehavior : MonoBehaviour {
+
+	public void ShowUIText(string text) {
+		GameObject panel = GameObject.Find ("textBox");
+		panel.GetComponent<Text> ().text = text;
+		RectTransform r = panel.GetComponent<RectTransform> ();
+		Vector2 pos = Input.mousePosition;
+		//r.offsetMin = new Vector2 (pos.x, r.offsetMin.y);
+		//r.offsetMax = new Vector2 (r.offsetMax.x, pos.y);
+		//r.anchoredPosition = pos;
+		//r.rect.position = pos;
+		r.position = gameObject.transform.position;
+	}
+
+	public void UnShowUIText(){
+		GameObject panel = GameObject.Find ("textBox");
+		panel.GetComponent<Text> ().text = "";
+	}
+}
